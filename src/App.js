247302419c5350a9a23b982d40e91b418b66a5a0
Kai Chang <kaijchang@gmail.com>
2019-04-20 23:13:35 -0700

    feat: start switching account creation to popup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React  from 'react';
 import Landing from './components/Landing';
-import CreateAccount from './components/CreateAccount';
 
 import { HashRouter, Route } from 'react-router-dom';
 
@@ -8,7 +7,6 @@ const App = () => (
     <HashRouter>
         <div>
             <Route path='/' exact component={ Landing }/>
-            <Route path='/createaccount' exact component={ CreateAccount }/>
         </div>
     </HashRouter>
 );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,29 +1,56 @@
-import React  from 'react';
-import { Table, Button } from 'reactstrap';
+import React, { useState } from 'react';
+import { Table, Button, Card, Input, ListGroup, ListGroupItem } from 'reactstrap';
 import Layout from './Layout';
 
-import { withRouter } from 'react-router-dom';
+import AccountTypes from '../implementations/Account';
 
-const Landing = ({ history }) => (
-    <Layout>
-        <Table style={ { marginTop: 25 } }>
-            <thead>
+const Landing = () => {
+    const [displayActive, setDisplayActive] = useState(false);
+    const [displayInputActive, setDisplayInputActive] = useState(false);
+    const [displayInput, setDisplayInput] = useState('');
+
+    return (
+        <Layout>
+            <Table style={{ marginTop: 25 }}>
+                <thead>
                 <tr>
                     <th>Name</th>
                     <th>Value</th>
                     <th>Last Updated</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <tr>
                     <td colSpan={ 3 }>No Accounts Yet. Add One to Start!</td>
                 </tr>
-            </tbody>
-        </Table>
-        <Button onClick={ () => history.push('/createaccount') }>
-            + Add Account
-        </Button>
-    </Layout>
-);
+                </tbody>
+            </Table>
+            <Button onClick={ () => setDisplayActive(!displayActive) }>
+                + Add Account
+            </Button>
+            {
+                displayActive ? <Card style={ { width: '50%', padding: 25 } }>
+                    <Input value={ displayInput } onChange={ e => setDisplayInput(e.target.value) } onFocus={ () => setDisplayInputActive(true) } placeholder='Account Type'/>
+                    {
+                        displayInputActive ? <ListGroup style={ { position: 'absolute', zIndex: 1, width: 'calc(100% - 50px)', marginTop: 38 } }>
+                            {
+                                Object.keys(AccountTypes)
+                                    .filter(type => type.toLowerCase().includes(displayInput.toLowerCase()))
+                                    .slice(0, 5)
+                                    .map((type, idx) =>
+                                        <ListGroupItem onClick={ () => {
+                                            setDisplayInput(type);
+                                            setDisplayInputActive(false);
+                                        } } key={ idx } style={ { cursor: 'pointer'} }>{ type }</ListGroupItem>
+                                    )
+                            }
+                        </ListGroup> : null
+                    }
+                    <Button disabled={ true } style={ { width: '25%', marginTop: 5 } }>Create</Button>
+                </Card> : null
+            }
+        </Layout>
+    );
+};
 
-export default withRouter(Landing);
+export default Landing;
